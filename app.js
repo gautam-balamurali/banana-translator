4e7afad0a102be16176188c3c7d6b52ebe102950
Gautam Balamurali <gtmbalamurali@gmail.com>
2022-12-19 00:41:28 +0530

    feat:beautyfication, refactoring and optimization

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 let textInput = document.querySelector("#txt-input");
 let button = document.querySelector("#btn-translate");
 let output = document.querySelector("#output");
+let copyrightText = document.querySelector("#copyrt-txt");
 
 let serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
-let getTranslationUrl = (input) => serverUrl.concat("?text=", input);
+/**
+ * Function to get banana translation url
+ * @param input
+ * @returns translation url
+ */
+let getBananaTranslationUrl = (input) => serverUrl.concat("?text=", input);
 
+/**
+ * Funtion to handle error
+ * @param error
+ */
 function errorHandler(error) {
   console.error(error);
   alert("Something went wrong with our server! Try again after some time");
 }
 
+/**
+ * Function to handle click event
+ */
 function clickHandler() {
-  fetch(getTranslationUrl(textInput.value))
+  fetch(getBananaTranslationUrl(textInput.value))
     .then((response) => response.json())
     .then((jsonData) => {
       let translatedText = jsonData.contents.translated;
@@ -21,4 +34,13 @@ function clickHandler() {
     .catch(errorHandler);
 }
 
+/**
+ * To make the copyright year generic
+ */
+let getCurrentYear = new Date().getFullYear();
+copyrightText.innerText = `© ${getCurrentYear} | All Rights Reserved`;
+
+/**
+ * Listening to the click event
+ */
 button.addEventListener("click", clickHandler);
